e813a95760ab3cf8f0f307fa087d993d375d7c8c
Sonatai <gebauerjenny@yahoo.de>
2023-11-08 19:19:19 +0100

    dockerize frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    server: {
+        port: 5000,
+        strictPort: true,
+        host: true,
+        origin: 'http://0.0.0.0:5000',
+    },
+    plugins: [react()],
+});
